c66ba7fad2f7ea983f29efe4a65fdad9346291e5
zmasa <65679922+zmmmasa@users.noreply.github.com>
2020-07-24 18:25:37 +0800

    Update txnews2.js

--- txnews2.js
+++ txnews2.js
@@ -296,7 +296,7 @@ return new Promise((resolve, reject) => {
 function showmsg() {
  return new Promise((resolve, reject) => {
   if(readnum&&videonum){
-   detail = signinfo+`` + `【文章阅读】已读/再读: `+ readnum +`/`+readtitle+` 篇\n`+`【阅读红包】已开/总计: `+openreadred+`/`+readredtotal+` 个🧧\n`+ `【观看视频】已看/再看: `+ videonum +`/`+videotitle+` 分钟\n`+`【视频红包】已开/总计: `+openvideored+`/`+videoredtotal+` 个🧧\n【每日一句】`+Dictum\n`+`腾讯新闻魔改不开红包版本 
+     detail = signinfo+redpackres + `【文章阅读】已读/再读: `+ readnum +`/`+readtitle+` 篇\n`+`【阅读红包】已开/总计: `+openreadred+`/`+readredtotal+` 个🧧\n`+ `【观看视频】已看/再看: `+ videonum +`/`+videotitle+` 分钟\n`+`【视频红包】已开/总计: `+openvideored+`/`+videoredtotal+` 个🧧\n【每日一句】`+Dictum 
   }
   sy.log(subTile+`\n`+detail)
    if
